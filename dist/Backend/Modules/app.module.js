06d1c7d102b3e2191232033ca55e7e99bb50100e
Sinat Lek <sinatlek026@gmail.com>
2026-06-02 13:00:00 +0700

    fix:car

--- dist/Backend/Modules/app.module.js
+++ dist/Backend/Modules/app.module.js
@@ -1,11 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerModules = void 0;
-<<<<<<< HEAD
-const admin_module_1 = require("./admin.module");
-const cart_module_1 = require("./cart.module");
-=======
->>>>>>> 116a67043ace5b74e50389c26bac5db65f6b5495
 const auth_module_1 = require("./auth.module");
 const catalog_module_1 = require("./catalog.module");
 const category_module_1 = require("./category.module");
@@ -13,16 +8,9 @@ const inventory_module_1 = require("./inventory.module");
 const order_module_1 = require("./order.module");
 const review_module_1 = require("./review.module");
 const user_module_1 = require("./user.module");
-const wishlist_module_1 = require("./wishlist.module");
 const registerModules = (app) => {
     (0, auth_module_1.registerAuthModule)(app);
     (0, user_module_1.registerUserModule)(app);
-<<<<<<< HEAD
-    (0, cart_module_1.registerCartModule)(app);
-    (0, wishlist_module_1.registerWishlistModule)(app);
-    (0, admin_module_1.registerAdminModule)(app);
-=======
->>>>>>> 116a67043ace5b74e50389c26bac5db65f6b5495
     (0, catalog_module_1.registerCatalogModule)(app);
     (0, category_module_1.registerCategoryModule)(app);
     (0, inventory_module_1.registerInventoryModule)(app);
--- src/Backend/Modules/app.module.ts
+++ src/Backend/Modules/app.module.ts
@@ -1,10 +1,5 @@
 import type { Express } from "express";
 
-<<<<<<< HEAD
-import { registerAdminModule } from "./admin.module";
-import { registerCartModule } from "./cart.module";
-=======
->>>>>>> 116a67043ace5b74e50389c26bac5db65f6b5495
 import { registerAuthModule } from "./auth.module";
 import { registerCatalogModule } from "./catalog.module";
 import { registerCategoryModule } from "./category.module";
@@ -17,12 +12,6 @@ import { registerWishlistModule } from "./wishlist.module";
 export const registerModules = (app: Express): void => {
   registerAuthModule(app);
   registerUserModule(app);
-<<<<<<< HEAD
-  registerCartModule(app);
-  registerWishlistModule(app);
-  registerAdminModule(app);
-=======
->>>>>>> 116a67043ace5b74e50389c26bac5db65f6b5495
   registerCatalogModule(app);
   registerCategoryModule(app);
   registerInventoryModule(app);
